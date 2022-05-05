de90e4d02850c164108e71406bfc63546eb3dcaf
sarratavakoli <sarra.tavakoli@gmail.com>
2022-05-05 09:21:41 -0500

    added labs

---
+++ ConnectedSQL/ConnectedSQL/CategoryDomainModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectedSQL
+{
+    public class CategoryDomainModel
+    {
+        
+            public int CategoryId { get; set; }
+            public string CategoryName { get; set; }
+            public string CategoryDescription { get; set; }
+        
+            
+    }
+}
